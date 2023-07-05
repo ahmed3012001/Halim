11f09566db0a5e9ae9bb0a910455be3f1c3951a7
Ahmed <aabdelhalim961@gmail.com>
2023-07-05 20:42:11 +0300

    first

---
+++ Card.js
@@ -0,0 +1,56 @@
+function myFunction1(){
+    document.getElementById("span1").style.display = "block"; 
+    document.getElementById("span1").style.fontSize = "18px";
+    document.getElementById("span1").style.color = "black";
+}
+function myFunction2(){
+    document.getElementById("span1").style.display = "none"; 
+}
+function myFunction3(){
+    document.getElementById("span2").style.display = "block"; 
+    document.getElementById("span2").style.fontSize = "18px";
+    document.getElementById("span2").style.color = "black";
+}
+function myFunction4(){
+    document.getElementById("span2").style.display = "none"; 
+}
+function myFunction5(){
+    document.getElementById("span3").style.display = "block"; 
+    document.getElementById("span3").style.fontSize = "18px";
+    document.getElementById("span3").style.color = "black";
+}
+function myFunction6(){
+    document.getElementById("span3").style.display = "none"; 
+}
+function myFunction7(){
+    document.getElementById("span4").style.display = "block"; 
+    document.getElementById("span4").style.fontSize = "18px";
+    document.getElementById("span4").style.color = "black";
+}
+function myFunction8(){
+    document.getElementById("span4").style.display = "none"; 
+}
+function myFunction9(){
+    document.getElementById("span5").style.display = "block"; 
+    document.getElementById("span5").style.fontSize = "18px";
+    document.getElementById("span5").style.color = "black";
+}
+function myFunction10(){
+    document.getElementById("span5").style.display = "none"; 
+}
+function myFunction11(){
+    document.getElementById("span6").style.display = "block"; 
+    document.getElementById("span6").style.fontSize = "18px";
+    document.getElementById("span6").style.color = "black";
+}
+function myFunction12(){
+    document.getElementById("span6").style.display = "none"; 
+}
+function myFunction13(){
+    document.getElementById("span7").style.display = "block"; 
+    document.getElementById("span7").style.fontSize = "18px";
+    document.getElementById("span7").style.color = "black";
+}
+function myFunction14(){
+    document.getElementById("span7").style.display = "none"; 
+}
